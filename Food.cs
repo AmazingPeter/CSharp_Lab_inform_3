8deef5de3f58c9e6d4dbcb5d1af3acb54d68d430
AmazingPeter <schvyrov@yandex.ru>
2016-04-16 17:50:16 +0400

    Added files via upload

---
+++ Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sem4_3
+{
+    abstract class Food
+    {
+        public string name;
+        List<string> listOfIngredients;
+        public double price;
+
+        public Food(string name, List<string> listOfIngredients, double price)
+        {
+            this.name = name;
+            this.listOfIngredients = listOfIngredients;
+            this.price = price;
+        }
+
+        public string ListOfIngredients()
+        {
+            string ingredients = "";
+            foreach (string i in listOfIngredients)
+                ingredients += i + " ";
+            return ingredients;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + ListOfIngredients() + " " + price + " ";
+        }
+
+        public abstract double UnitPrice();//Цена за единицу
+
+        public abstract int IsAlcoholic();
+
+        public abstract int IsDietary();
+    }
+}
